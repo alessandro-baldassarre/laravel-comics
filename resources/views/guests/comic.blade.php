d6003ec6c72a1e75618aa248d2626cfdb7516801
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-05 18:04:47 +0200

    completed page comic detail

--- resources/views/guests/comic.blade.php
+++ resources/views/guests/comic.blade.php
@@ -15,61 +15,70 @@
             </div>
         </div>
       </div>
-      <div class="my-container">
         <div class="comic-main">
+          <div class="comic-main-top">
+                <div class="comic-description">
+                <h1>{{ $comic['title']}}</h1>
+                <div class="price">
+                  <div class="price-left">
+                      <p>U.S. Price: {{ $comic['price']}}</p>
+                      <span>available</span>
+                  </div>
+                  <span class="price-right">Check Availability</span>
+                </div>
 
-          <div class="comic-description">
-          <h1>{{ $comic['title']}}</h1>
-          <p>{{ $comic['price']}}</p>
-          <p>{{ $comic['description']}}</p>
+                <p>{{ $comic['description']}}</p>
+                </div>
+              <div class="advertisement">
+                <p>advertisement</p>
+                <img src="{{asset("assets/images/adv.jpg")}}" alt="{{$comic['title']}}" />
+              </div>
+          </div>
+            
+            
         </div>
-
-        <div class="advertisement">
-          <p>advertisement</p>
-          <img src="{{asset("assets/images/adv.jpg")}}" alt="{{$comic['title']}}" />
-        </div>
-
-        </div>
-    
         <div class="comic-details">
           <div class="talent">
+            <h5>Talent</h5>
             <div class="art">
-              <h5>Art by:</h5>
-              @foreach ($comic['artists'] as $index => $artist)
-                 @if (!$loop->last)
+              <span class="span-left">Art by:</span>
+              <span class="span-right">
+                  @foreach ($comic['artists'] as $index => $artist)
+                  @if (!$loop->last)
                     {{$artist.', '}}  
                   @endif
                     {{$artist}}
-              @endforeach
+                  @endforeach
+              </span>
             </div>
             <div class="written">
-              <h5>Written by:</h5>
-              @foreach ($comic['writers'] as $index => $writer)
+              <span class="span-left">Written by:</span>
+              <span class="span-right">
+                @foreach ($comic['writers'] as $index => $writer)
                  @if (!$loop->last)
                     {{$writer.', '}}  
                   @endif
                     {{$writer}}
               @endforeach
+              </span>
             </div>
           </div>
-          <div class="Specs">
+          <div class="specs">
+            <h5>Specs</h5>
             <div class="series">
-              <h5>Series</h5>
-              <p>{{$comic['series']}}</p>
+              <span class="span-left">Series</span>
+              <span class="span-right">{{$comic['series']}}</span>
             </div>
             <div class="price">
-              <h5>U.S Price:</h5>
-              <p>{{$comic['price']}}</p>
+              <span class="span-left">U.S Price:</span>
+              <span class="span-right">{{$comic['price']}}</span>
             </div>
             <div class="sale-date">
-              <h5>On Sale Date:</h5>
-              <p>{{$comic['sale_date']}}</p>
+              <span class="span-left">On Sale Date:</span>
+              <span class="span-right">{{$comic['sale_date']}}</span>
             </div>
           </div>
         </div>
-    
-      </div>
     </div>
 </div>
-     @dump($comic)
 @endsection
