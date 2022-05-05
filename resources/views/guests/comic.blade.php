c9d77aef8f58c79fc784adc434a5ee34e4bc010e
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-05 16:34:35 +0200

    realized style top single page comic

--- resources/views/guests/comic.blade.php
+++ resources/views/guests/comic.blade.php
@@ -5,20 +5,71 @@
 <div id="jumbotron"></div>
 
 <div>
-    <div class="products">
-      <a id="current-button" href="#">current series</a>
-      <div class="my-container">
-
-            <div class="product-card">
-                <div class="img-card">
-                <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
-                </div>
-                <h4>{{ $comic['series']}}</h4>
-               
+    <div class="comic">
+      <div class="comic-top">
+        <div class="my-container">
+          <div class="img-card">
+            <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
+            <span class="type">{{$comic['type']}}</span>
+            <span class="gallery">view gallery</span>
             </div>
- @dump($comic)
+        </div>
+      </div>
+      <div class="my-container">
+        <div class="comic-main">
+
+          <div class="comic-description">
+          <h1>{{ $comic['title']}}</h1>
+          <p>{{ $comic['price']}}</p>
+          <p>{{ $comic['description']}}</p>
+        </div>
+
+        <div class="advertisement">
+          <p>advertisement</p>
+          <img src="{{asset("assets/images/adv.jpg")}}" alt="{{$comic['title']}}" />
+        </div>
+
+        </div>
+    
+        <div class="comic-details">
+          <div class="talent">
+            <div class="art">
+              <h5>Art by:</h5>
+              @foreach ($comic['artists'] as $index => $artist)
+                 @if (!$loop->last)
+                    {{$artist.', '}}  
+                  @endif
+                    {{$artist}}
+              @endforeach
+            </div>
+            <div class="written">
+              <h5>Written by:</h5>
+              @foreach ($comic['writers'] as $index => $writer)
+                 @if (!$loop->last)
+                    {{$writer.', '}}  
+                  @endif
+                    {{$writer}}
+              @endforeach
+            </div>
+          </div>
+          <div class="Specs">
+            <div class="series">
+              <h5>Series</h5>
+              <p>{{$comic['series']}}</p>
+            </div>
+            <div class="price">
+              <h5>U.S Price:</h5>
+              <p>{{$comic['price']}}</p>
+            </div>
+            <div class="sale-date">
+              <h5>On Sale Date:</h5>
+              <p>{{$comic['sale_date']}}</p>
+            </div>
+          </div>
+        </div>
+    
       </div>
     </div>
 </div>
-    
+     @dump($comic)
 @endsection
